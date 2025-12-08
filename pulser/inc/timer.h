448d020f3552d47db213cdc1055bdce223337a4b
keith <keithlegg23@gmail.com>
2025-12-08 11:24:31 -0800

    python is borken - avoid looking at timers and threads

---
+++ pulser/inc/timer.h
@@ -0,0 +1,69 @@
+#ifndef TIMER_H    
+#define TIMER_H
+
+
+#include <sys/time.h>
+
+void timer_test(void);
+
+
+
+
+// time_t         tv_sec      seconds
+// suseconds_t    tv_usec     microseconds
+// The <sys/time.h> header defines the itimerval structure that includes at least the following members:
+// struct timeval it_interval timer interval
+// struct timeval it_value    current value
+
+
+/*
+   FROM HERE 
+
+   http://www.songho.ca/misc/timer/timer.html
+ 
+*/
+
+
+class timer
+{
+    public:
+        
+        timer(){
+            startCount.tv_sec = startCount.tv_usec = 0;
+            endCount.tv_sec = endCount.tv_usec = 0;
+
+            stopped = 0;
+            startTimeInMicroSec = 0;
+            endTimeInMicroSec = 0;
+        }
+
+        ~timer(){};
+
+
+        void   start();                             // start timer
+        void   stop();                              // stop the timer
+        double getElapsedTime();                    // get elapsed time in second
+        double getElapsedTimeInSec();               // get elapsed time in second (same as getElapsedTime)
+        double getElapsedTimeInMilliSec();          // get elapsed time in milli-second
+        double getElapsedTimeInMicroSec();          // get elapsed time in micro-second
+
+    protected:
+
+    private:
+        double startTimeInMicroSec;                 // starting time in micro-second
+        double endTimeInMicroSec;                   // ending time in micro-second
+        int    stopped;                             // stop flag 
+
+        timeval startCount;                         
+        timeval endCount;                            
+
+
+
+};
+
+
+
+
+
+
+#endif
---
+++ pulser/src/timer.cpp
@@ -0,0 +1,107 @@
+// #include <stdlib.h>
+// #include <stdio.h>
+
+#include <chrono>
+#include <iostream>
+
+
+#include "timer.h"
+
+
+/*
+int main() {                         
+    high_resolution_clock::time_point t1 = high_resolution_clock::now(); 
+    high_resolution_clock::time_point t2 = high_resolution_clock::now();  
+    duration<double> time_span = duration_cast<duration<double>>(t2 - t1);      
+    std::cout << time_span.count() << " seconds\n";
+    return 0; 
+} 
+*/
+
+
+
+
+
+
+ 
+
+
+
+
+// void timer_test(void)
+// {
+// 
+//     timeval startCount;
+//     float foo = gettimeofday(&startCount, NULL);
+//     printf("%ld.%06ld\n", startCount.tv_sec, startCount.tv_usec);
+// 
+// };
+
+
+
+
+void timer::start()
+{
+    stopped = 0; // reset stop flag
+
+    gettimeofday(&startCount, NULL);
+
+}
+
+
+
+
+void timer::stop()
+{
+    stopped = 1; // set timer stopped flag
+
+
+    gettimeofday(&endCount, NULL);
+
+}
+
+
+
+
+double timer::getElapsedTimeInMicroSec()
+{
+
+    if(!stopped)
+        gettimeofday(&endCount, NULL);
+
+    startTimeInMicroSec = (startCount.tv_sec * 1000000.0) + startCount.tv_usec;
+    endTimeInMicroSec = (endCount.tv_sec * 1000000.0) + endCount.tv_usec;
+
+    return endTimeInMicroSec - startTimeInMicroSec;
+}
+
+
+
+
+double timer::getElapsedTimeInMilliSec()
+{
+    return this->getElapsedTimeInMicroSec() * 0.001;
+}
+
+
+
+
+double timer::getElapsedTimeInSec()
+{
+    return this->getElapsedTimeInMicroSec() * 0.000001;
+}
+
+
+
+
+double timer::getElapsedTime()
+{
+    return this->getElapsedTimeInSec();
+}
+
+
+
+
+
+
+
